4541f2d5118aedd67a217194375b7f90951ef9e4
consti <consti@consti.de>
2015-04-03 10:37:56 +0700

    fix title

--- config.rb
+++ config.rb
@@ -31,15 +31,12 @@ end
 # Helpers
 #-------------------------------------------------------------------------------
 helpers do
-
   # Given a default_title string, return a string in the format
   # "title | default_title", where title is the string defined by frontmatter.
   #
   # @param  {String} - default_title
   # @return {String}
-  def page_title(default_title)
-    title = current_page.data.title
-    if title then "#{title} | #{default_title}" else default_title end
+  def page_title
+    current_page.data.title
   end
-
 end
